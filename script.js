eec32b6993c41f8c83ca5d59627db5ed5285f4a4
rbundy <reid.bundy.il@gmail.com>
2020-02-22 16:45:39 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById("joke-btn").addEventListener("click", function(event) {
+    document.getElementById("joke-btn").disabled = true;
+    event.preventDefault();
+
+    document.getElementById("setup").innerHTML = ""
+    document.getElementById("punchline").innerHTML = ""
+
+    const url = "https://official-joke-api.appspot.com/jokes/programming/random"
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        }).then(async function(json) {
+            await typeWriter(json[0].setup, "setup")
+            await sleep(2500)
+            await typeWriter(json[0].punchline, "punchline")
+            document.getElementById("joke-btn").disabled = false;
+        });
+});
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+async function typeWriter(txt, id) {
+    var i = 0;
+    var speed = 50;
+    while (i < txt.length) {
+        document.getElementById(id).innerHTML += txt.charAt(i);
+        i++;
+        await sleep(speed)
+    }
+}
